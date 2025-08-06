5809ceabacba32256f1dff58f74fc67c31211d09
dev-gameric <eric.silva.de.souza.93.com@gmail.com>
2025-08-06 14:57:26 -0300

    feature: cria BaseEntity e anota @EnableJpaRepositories na main

--- plano_de_saude/src/main/java/com/projects/desafio/plano_de_saude/PlanoDeSaudeApplication.java
+++ plano_de_saude/src/main/java/com/projects/desafio/plano_de_saude/PlanoDeSaudeApplication.java
@@ -2,8 +2,10 @@ package com.projects.desafio.plano_de_saude;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class PlanoDeSaudeApplication {
 
 	public static void main(String[] args) {
