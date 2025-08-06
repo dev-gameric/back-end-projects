79960c5e29fcacf67aeaf7b12071284c1911e698
dev-gameric <eric.silva.de.souza.93.com@gmail.com>
2025-08-06 12:38:43 -0300

    initial commit: projeto plano de saude

---
+++ plano_de_saude/src/main/java/com/projects/desafio/plano_de_saude/PlanoDeSaudeApplication.java
@@ -0,0 +1,13 @@
+package com.projects.desafio.plano_de_saude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoDeSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoDeSaudeApplication.class, args);
+	}
+
+}
---
+++ plano_de_saude/src/test/java/com/projects/desafio/plano_de_saude/PlanoDeSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.desafio.plano_de_saude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoDeSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
